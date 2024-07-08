1c302dcb6abc92e13970866f5df14e59bdf8cf18
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-07-07 22:53:27 -0500

    Fix build configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches the directory your server is configured to serve
+  },
   server: {
     port: 3000,
     open: true,
