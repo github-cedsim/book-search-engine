607cb010cc7546ee700717ae3ad1c7f2eccb0dbd
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-07-07 23:17:55 -0500

    Fix static file serving for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      plugins: [react()],
-      build: {
-        outDir: 'dist'
-      },
-      server: {
-        port: 3000,
-        open: true,
-        proxy: {
-          '/api': {
-            target: 'http://localhost:3001',
-            secure: false,
-            changeOrigin: true
-          }
-        }
-      }
-    }
-  } else {
-    return {
-      plugins: [react()],
-      build: {
-        outDir: 'dist'
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        secure: false,
+        changeOrigin: true
       }
     }
   }
