9b46bc347ea632926638d316c08a6b38d87746c3
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-07-07 23:06:07 -0500

    chore: Update Vite configuration for serving and building the client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this matches the directory your server is configured to serve
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist'
+      },
+      server: {
+        port: 3000,
+        open: true,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3001',
+            secure: false,
+            changeOrigin: true
+          }
+        }
+      }
+    }
+  } else {
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist'
       }
     }
   }
