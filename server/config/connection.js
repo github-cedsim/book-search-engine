12c62554a02061bacc541d213e62802d25e5a247
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-07-07 21:41:17 -0500

    Update MongoDB connection string for Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://cedsim:NYexNRP9NE0vwXwa@booksearchdb.wo1zefd.mongodb.net/?retryWrites=true&w=majority&appName=booksearchdb';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
